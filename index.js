0aab6fcb8a3125d38e6c2b0d42ab36684508f2ff
alberto1189 <alberto.corvo89@gmail.com>
2019-09-09 10:53:40 +0200

    edits-5

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
 
 
 //src/index.js
-export { default as TestComponent } from './src/TestComponent';
+//export { default as TestComponent } from './src/TestComponent';
+
+
+export { default as TodaysDate } from './src/TodayDate';
---
+++ src/TodayDate.js
@@ -0,0 +1,9 @@
+
+
+
+//src/TodaysDate.js
+import React from "react";
+const TodaysDate = () =>
+    <div>{`Today's date is ${Date()}`}</div>;
+export default TodaysDate;
+
