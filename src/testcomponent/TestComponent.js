467cb3fae7c8348d82b029fae22e5540650ecaad
alberto1189 <alberto.corvo89@gmail.com>
2019-10-10 16:03:01 +0200

    updates

---
+++ src/testcomponent/TestComponent.js
@@ -0,0 +1,20 @@
+
+import React, {Component} from "react";
+
+class TestComponent extends Component {
+
+    constructor(props)
+    {
+        super(props)
+    }
+    render() {
+        return (<div>{"My APP name"}</div>)
+
+
+    }
+}
+
+
+export default TestComponent;
+
+
---
+++ src/testcomponent/index.js
@@ -0,0 +1,10 @@
+
+
+
+
+//src/index.js
+//export { default as TestComponent } from './src/TestComponent';
+
+
+
+export { default as TestComponent } from './TestComponent';
